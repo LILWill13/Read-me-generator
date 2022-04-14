2e341e75bc54f51ec8faa7c6e35aefda7caced97
noah gaston <noahsfuture18@gmail.com>
2022-04-14 17:03:04 -0400

    created prompt

---
+++ Develop/index.js
@@ -0,0 +1,68 @@
+// // TODO: Include packages needed for this application
+// const fs = require('fs')
+// const inquirer = require('inquirer')
+// // TODO: Create an array of questions for user input
+// const questions = [
+
+// ];
+
+// inquirer
+//     .prompt ([
+//     {
+//         type: 'input',
+//         message: 'What is the your github username?',
+//         name: 'username',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is the your email?',
+//         name: 'email',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is the title of your project?',
+//         name: 'title',
+//     },
+//     {
+//         type: 'input',
+//         message: 'Give a discription of your project.',
+//         name: 'discription',
+//     },
+//     {
+//         type: 'input',
+//         message: 'How to Install your project',
+//         name: 'instalation',
+//     },
+//     {
+//         type: 'input',
+//         message: 'How would your project be used',
+//         name: 'usage',
+//     },
+//     {
+//         type: 'input',
+//         message: 'Who contributed to your project',
+//         name: 'contribution',
+//     },
+//     {
+//         type: 'input',
+//         message: 'Test Instructions',
+//         name: 'tests',
+//     },
+//     {
+//         type: 'list',
+//         message: 'What licenses would you like to your project?',
+//         choices: ['IBM', 'MIT', 'Mozilla', 'Zlib'],
+//         name: 'license',
+//     },
+
+// ]).then((answers) => console.log(answers))
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
